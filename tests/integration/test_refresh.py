643a40d0aea12af202b2785c8cc77b60f2649bf1
Tim Couper <drtimcouper@gmail.com>
2016-02-17 12:47:54 +0000

    tidied up the expected.. files cfreated in testing refresh

--- tests/integration/test_refresh.py
+++ tests/integration/test_refresh.py
@@ -1,5 +1,6 @@
 import os
 import json
+import shutil
 
 from nose.tools import assert_equal, assert_raises
 from unittest.mock import patch
@@ -18,28 +19,36 @@ def test_csv_does_not_exist():
 
 
 def test_ds_duplicate_name():
-    with patch.object(refresh, 'get_content_from_url') as get_content:
-        with patch.object(Refresh, 'save_content'):
-            my_content = {'WMS1': 'my sentinel'}
-            get_content.return_value = my_content
-            ref = Refresh(os.path.join(DATA_DIR, 'duplicate_name.csv'))
-            ref.run()
-            assert_equal(get_content.call_count,1)
-            assert_equal(len(ref.errors), 1)
-            assert_equal(ref.errors[0].args, ('python-site',))
+    try:
+        with patch.object(refresh, 'get_content_from_url') as get_content:
+            with patch.object(Refresh, 'save_content'):
+                my_content = {'WMS1': 'my sentinel'}
+                get_content.return_value = my_content
+                ref = Refresh(os.path.join(DATA_DIR, 'duplicate_name.csv'))
+                ref.run()
+                assert_equal(get_content.call_count,1)
+                assert_equal(len(ref.errors), 1)
+                assert_equal(ref.errors[0].args, ('python-site',))
+    finally:
+        # tidy up the created "expected..." directory
+        shutil.rmtree(os.path.join(DATA_DIR, 'expected_duplicate_name'), ignore_errors=True)
 
 
 def test_ds_simple():
-    with patch.object(refresh, 'get_content_from_url') as get_content:
-        my_content = {'WMS1': 'my sentinel'}
-        get_content.return_value = my_content
-        ref = Refresh(os.path.join(DATA_DIR, 'simple.csv'))
-        ref.run()
-        files_present = os.listdir(ref.expected_dir)
-        assert_equal(len(files_present), 1)
+    try:
+        with patch.object(refresh, 'get_content_from_url') as get_content:
+            my_content = {'WMS1': 'my sentinel'}
+            get_content.return_value = my_content
+            ref = Refresh(os.path.join(DATA_DIR, 'simple.csv'))
+            ref.run()
+            files_present = os.listdir(ref.expected_dir)
+            assert_equal(len(files_present), 1)
 
-        the_file = os.path.join(ref.expected_dir, files_present[0])
-        with open(the_file) as f:
-            body = f.read()
+            the_file = os.path.join(ref.expected_dir, files_present[0])
+            with open(the_file) as f:
+                body = f.read()
 
-        assert_equal(json.loads(body), my_content)
+            assert_equal(json.loads(body), my_content)
+    finally:
+            # tidy up the created "expected..." directory
+            shutil.rmtree(os.path.join(DATA_DIR, 'expected_simple'), ignore_errors=True)
