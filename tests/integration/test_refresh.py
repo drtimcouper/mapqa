42d50c76596010d79619a761f0e1122f6630f533
Tim Couper <drtimcouper@gmail.com>
2016-02-17 10:56:34 +0000

    added simple.csv test

--- tests/integration/test_refresh.py
+++ tests/integration/test_refresh.py
@@ -17,7 +17,7 @@ class Test_build:
         with assert_raises(IOError):
            Refresh('junk')
 
-    def test_duplicate_name(self):
+    def test_ds_duplicate_name(self):
         with patch.object(refresh, 'get_content') as get_content:
             with patch.object(Refresh, 'save_content'):
                 ref = Refresh(os.path.join(DATA_DIR, 'duplicate_name.csv'))
@@ -25,4 +25,16 @@ class Test_build:
                 assert_equal(len(ref.errors), 1)
                 assert_equal(ref.errors[0].args, ('python-site',))
 
+    def test_ds_simple(self):
+        with patch.object(refresh, 'get_content') as get_content:
+            get_content.return_value = 'my sentinel'
+            ref = Refresh(os.path.join(DATA_DIR, 'simple.csv'))
 
+            files_present = os.listdir(ref.expected_dir)
+            assert_equal(len(files_present), 1)
+
+            the_file = os.path.join(ref.expected_dir, files_present[0])
+            with open(the_file) as f:
+                body = f.read()
+
+            assert_equal(body, 'my sentinel')
